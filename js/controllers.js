cb5bccdc142f7f91b75c166052496503df0445a4
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-17 17:07:08 +0100

    ready to move into laravel to get an api working :)

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-
+(function(){
 
   var freddieApp = angular.module("freddieApp", []);
 
@@ -9,9 +9,22 @@
     $http.get('api/chat.json').success(function(data) {
 
       $scope.messages = data;
-      
+
     });
+
+    $scope.submit = function() {
+
+      formData = $scope.form;
+
+      $http.post('api/chat.json', formData).success(function(data) {
+
+        alert('hi');
+
+      });
+      
+    };
     
 
   });
 
+})();
